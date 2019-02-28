bdced046e89d56d51c389aa7bc268fc4dcef1bc4
Akash Verma <akashvermapro@gmail.com>
2019-02-28 10:32:03 +0530

    Modified settings.py

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT),
+        'PORT': config('DB_PORT'),
     }
 }
 
