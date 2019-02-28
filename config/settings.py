088776bcdf48ec4918a4e612b2c7d2c8d8a30d58
Akash Verma <akashvermapro@gmail.com>
2019-02-28 09:46:45 +0530

    Accidently deleted all migrations

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'onlinemechaniclocator_db',
+        'USER': 'postgres',
+        'PASSWORD': 'mnblkjpoi',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
